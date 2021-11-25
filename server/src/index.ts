b7b0a99e6076ce5d3d81d6a92345c646944558a6
Syncxv <aruldevarackal1@gmail.com>
2021-11-26 04:54:28 +0530

    express boilerplate :P

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,10 @@
-console.log('hehe')
+import express from 'express'
+const PORT = 8000
+const main = async () => {
+    const app = express()
+    app.listen(PORT, () =>
+        console.log(`listening on port ${PORT} url: http://localhost:${PORT}`)
+    )
+}
+
+main().catch(err => console.error(err))
